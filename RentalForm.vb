199b4f13b2ece5d11ea34635b1a07b94887f1b3a
Zac <zac.minidude140@Gmail.com>
2023-11-16 09:13:22 -0700

    added initial user input validation need to reorder response

--- RentalForm.vb
+++ RentalForm.vb
@@ -7,6 +7,38 @@
 Option Explicit On
 Option Strict On
 Option Compare Binary
-Public Class RentalForm
 
+'TODO
+'[]fix order of textbox response
+Public Class RentalForm
+    'Custom Methods
+
+    ''' <summary>
+    ''' Checks that each text field has something entered
+    ''' </summary>
+    Sub ValidateUserInput()
+        Dim isValid As Boolean = True
+        Dim errorMessage As String
+        'checks if each text box is empty
+        For Each Item As TextBox In CustomerInputGroupBox.Controls.OfType(Of TextBox)
+            If isValid = True Then
+                Item.Focus()
+            End If
+            If Item.Text = "" Then
+                isValid = False
+                errorMessage &= Replace($"{Item.Name.ToString} is required{vbCrLf}", "TextBox", "")
+            End If
+        Next
+        'Message the user if an error has occurred
+        If errorMessage <> "" Then
+            MsgBox(errorMessage)
+        End If
+    End Sub
+    'Event Handlers
+    Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
+        Me.Close()
+    End Sub
+    Private Sub CalculateButton_Click(sender As Object, e As EventArgs) Handles CalculateButton.Click
+        ValidateUserInput()
+    End Sub
 End Class
