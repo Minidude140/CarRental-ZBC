cdb3b5e3b3c6fab10b575686162ad2b2bc2c8352
Zac <zac.minidude140@Gmail.com>
2023-11-21 10:00:19 -0700

    Added TODO from instructions list

--- RentalForm.vb
+++ RentalForm.vb
@@ -10,7 +10,37 @@ Option Compare Binary
 
 'TODO
 '[~]fix order of text box response
-'[]
+'[]Finish user input validation **See Below**
+'{}checkOdometer *beginning Odometer should not be greater than ending odemeter
+'{}CheckDays *day greater than 0 no more than 45
+
+'[]Calculations:
+'[]Daily Charge is 15$ per day
+'[]mileage: first 200mi free, 201-500 12cents/mi, > 500mi 10cents/mi
+'{}all calc done in miles convert if Kilometer radio button is selected
+'{}1 Kilometer = 0.62 miles
+'{}if reading in kilometers return in kilometers
+'[]AAA members receive 5% discount
+'[]senoir citizens receive 3% discount
+'{}both discounts can be used as once
+
+'[]Display:
+'{}distance traveled in given units
+'{}total millage charge as currency
+'{}total daily charge as currency
+'{}total discount as currency
+'{}total charges as currency
+
+'[]Summary:
+'{}only display if at least one rental has been completed
+'{}display total # of customers
+'{}display total distance in miles
+'{}display total charges
+'{}clear form **do not clear summary totals**
+
+'[]Set Defaults and Clear
+'[]Add Close program confirmation box
+
 Public Class RentalForm
     'Custom Methods
 
